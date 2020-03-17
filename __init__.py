933700946a89e853ae95d490be4c3c8fd2038970
BlueGreenMagick <bluegreenmagick@gmail.com>
2020-03-18 08:47:42 +0900

    import AnkiQt from aqt.main because in older versions, AnkiQt is not imported in global namespace in __init__.py

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ import time
 from datetime import datetime, timedelta
 from pathlib import Path
 
-from aqt import AnkiQt
+from aqt.main import AnkiQt
 from anki.hooks import wrap, addHook
 from anki.utils import intTime
 try:
